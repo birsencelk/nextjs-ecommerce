23b2b69b0f29c8fc0846ac34d3994a354c91dad8
birsencelk <birsencelk@gmail.com>
2026-01-03 11:52:44 +0300

    feat(app): implement Navbar component with navigation links

---
+++ app/navbar.tsx
@@ -0,0 +1,27 @@
+import Link from "next/link";
+
+export default function Navbar() {
+  return (
+    <nav className="bg-white shadow-md">
+      <div className="container mx-auto px-4 py-3 flex justify-between items-center">
+        <ul className="flex space-x-4">
+          <li>
+            <Link href="/" className="text-gray-700 hover:text-black">
+              Home Page
+            </Link>
+          </li>
+          <li>
+            <Link href="/cart" className="text-gray-700 hover:text-black">
+              Shopping Cart
+            </Link>
+          </li>
+          <li>
+            <Link href="/checkout" className="text-gray-700 hover:text-black">
+              Checkout
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
